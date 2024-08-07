a1959488e66de8f96fb825758ac69a907dcfe504
Jayson <fatloser420@gmail.com>
2024-08-07 14:37:03 -0400

    changed file paths to be const variables

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 use rand::seq::{IteratorRandom, SliceRandom};
 use rand::Rng;
+use rodio::{source::Source, Decoder, OutputStream};
 use std::fs;
-use std::io::{self, BufRead, BufReader, stdout, Write};
-use rodio::{Decoder, OutputStream, source::Source};
+use std::io::{self, stdout, BufRead, BufReader, Write};
+
+const SCRIPT_PATH: &str = "/home/jayson/draculations/src/script.txt";
+const SFX_PATH: &str = "/home/jayson/draculations/sfx/";
+
 
 fn main() -> io::Result<()> {
-
-    let script_path = "/home/jayson/draculations/src/script.txt";
     let _sfx = set_sfx();
     let mut rng = rand::thread_rng();
 
-    let _script = fs::File::open(&script_path)?;
+    let _script = fs::File::open(SCRIPT_PATH)?;
     let reader = io::BufReader::new(_script);
-    
+
     let lines: Vec<String> = reader.lines().collect::<Result<_, _>>()?;
     let Some(bar) = lines.choose(&mut rng) else {
         todo!()
@@ -20,7 +22,7 @@ fn main() -> io::Result<()> {
 
     println!("{}", '\n');
 
-    for c in bar.chars(){
+    for c in bar.chars() {
         print!("{}", c);
         let _ = stdout().flush();
         let _ = play_sfx(_sfx.clone());
@@ -31,14 +33,14 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn set_sfx() -> String{
+fn set_sfx() -> String {
     let mut rng = rand::thread_rng();
-    let sounds = fs::read_dir("/home/jayson/draculations/sfx/").unwrap();
+    let sounds = fs::read_dir(SFX_PATH).unwrap();
     let sfx_path = sounds.choose(&mut rng).unwrap().unwrap();
     return sfx_path.path().display().to_string();
 }
 
-fn play_sfx(_sfx: String) -> io::Result<()>{
+fn play_sfx(_sfx: String) -> io::Result<()> {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let _delay = rand::thread_rng().gen_range(40..200);
 
