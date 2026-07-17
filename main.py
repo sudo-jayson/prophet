c0026e8e81dd359c2a67170793d30accae66e28e
Jay <fatloser420@gmail.com>
2026-07-17 16:37:29 -0400

    it works?

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time, random, playsound3, os, sys
+from pathlib import PurePosixPath, Path
 
 ##Takes input from user and uses it to determine which script and shich sounds to use
 def getProphet(question):
@@ -8,17 +9,19 @@ def getProphet(question):
     else:   return "test"
 
 ##Instantiates the variables for script and sound effects based on config file
-configFile = os.getcwd() + '\\config\\' + getProphet(3) + '.conf'
+p = Path.cwd()
+configFile =PurePosixPath('config/' + getProphet(1) + '.conf')
 with open(configFile, 'r') as f:
     data = f.read().splitlines()
-    textFile = os.getcwd() + '\\config\\' + data[0] +'.txt'
-    sfxDir = os.getcwd() + '\\sfx\\' +data[1]
+    textFile = PurePosixPath('config/' + data[0] +'.txt')
+    sfxDir = PurePosixPath('sfx/' +data[1])
     minDelay = float(data[2])
     maxDelay = float(data[3])
 
 ##Gets list of sounds and picks one at random
 sfxList = os.listdir(sfxDir)
-sfx = sfxDir + "\\" + random.choice(sfxList)
+soundFile=random.choice(sfxList)
+sfx = PurePosixPath(sfxDir / soundFile)
 
 ##Gets location of text file and chooses a line at random from it
 def getText(textFile):
